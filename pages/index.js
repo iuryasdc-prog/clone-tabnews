851d1b1b0f85cba812e58072218586f0f1924c7d
iuryasdc-prog <iuryasdc@gmail.com>
2026-08-17 02:12:22 +0000

    Tag h2 adicionada.

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,9 @@ function Home(){
     return <h1>Podemos fazer melhor. :D</h1>
 }
 
+function HomeYet(){
+    return <h2>Realmente... Podemos fazer melhor do que isso!!!</h2>
+}
+
 export default Home;
+export default HomeYet;
