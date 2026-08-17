fa64ce67d174bb93b4eea855bb1ad3e621092678
iuryasdc-prog <iuryasdc@gmail.com>
2026-08-17 02:14:19 +0000

    tag trocada de h1 para h2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home(){
-    return <h1>Podemos fazer melhor. :D</h1>
-}
-
-function HomeYet(){
-    return <h2>Realmente... Podemos fazer melhor do que isso!!!</h2>
+    return <h2>Podemos fazer melhor. :D</h2>
 }
 
 export default Home;
-export default HomeYet;
